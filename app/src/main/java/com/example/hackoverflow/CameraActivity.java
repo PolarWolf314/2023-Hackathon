ceec423d19a7d9876b45cc718df31ec541f459e0
PolarWolf314 <aaronjoshguo@gmail.com>
2023-07-08 15:51:01 +1200

    cleanup - deleted unused code

--- app/src/main/java/com/example/hackoverflow/CameraActivity.java
+++ app/src/main/java/com/example/hackoverflow/CameraActivity.java
@@ -3,14 +3,8 @@ package com.example.hackoverflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import android.os.Bundle;
 
 public class CameraActivity extends AppCompatActivity {
-    Button btnpicture;
-    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
