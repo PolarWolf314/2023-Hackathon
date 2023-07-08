f4b3f3cc45ad6855dbfa098d251b5cece8dd0168
Anna Shimizu <ashi725@aucklanduni.ac.nz>
2023-07-08 22:21:00 +1200

    Navbar implemented

--- app/src/main/java/com/example/hackoverflow/ViewActivity.java
+++ app/src/main/java/com/example/hackoverflow/ViewActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ViewActivity extends AppCompatActivity {
 
@@ -14,17 +15,46 @@ public class ViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
 
-        Button viewBackButton = findViewById(R.id.viewBackButton);
-        viewBackButton.setOnClickListener(new View.OnClickListener() {
+        ImageView navbarHomeButton = (ImageView) findViewById(R.id.navbarHomeButton);
+        navbarHomeButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                onBackPressed();
+            public void onClick(View view){
+                Intent homeIntent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(homeIntent);
             }
         });
+
+        ImageView navbarSearchButton = (ImageView) findViewById(R.id.navbarSearchButton);
+        navbarSearchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent searchIntent = new Intent(getBaseContext(), SearchActivity.class);
+                startActivity(searchIntent);
+            }
+        });
+
+        ImageView navbarBookmarkButton = (ImageView) findViewById(R.id.navbarBookmarkButton);
+        navbarBookmarkButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent homeIntent = new Intent(getBaseContext(), BookmarkActivity.class);
+                startActivity(homeIntent);
+            }
+        });
+
+        ImageView navbarMenuButton = (ImageView) findViewById(R.id.navbarMenuButton);
+        navbarMenuButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent searchIntent = new Intent(getBaseContext(), MenuActivity.class);
+                startActivity(searchIntent);
+            }
+        });
+
+
     }
 
-    public void onBackPressed() {
-        Intent intent = new Intent(ViewActivity.this, MainActivity.class);
-        startActivity(intent);
-    }
+
+
+
 }
