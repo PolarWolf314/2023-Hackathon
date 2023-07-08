97a3c75af18b4c9fb27011b424cd9ee073104638
PolarWolf314 <aaronjoshguo@gmail.com>
2023-07-08 22:14:27 +1200

    feat - api is working

--- app/src/main/java/com/example/hackoverflow/api.java
+++ app/src/main/java/com/example/hackoverflow/api.java
@@ -1,49 +1,56 @@
 package com.example.hackoverflow;
 
-import okhttp3.*;
-
 import java.io.File;
 import java.io.IOException;
 
+import okhttp3.*;
+
+import android.os.AsyncTask;
+
 public class api {
     private static final String ORGAN = "auto";
+    private static final String PROJECT = "all";
+    private static final String URL = "https://my-api.plantnet.org/v2/identify/" + PROJECT + "?api-key=2b103ZERjEsjWMFUuTa2onvT";
 
-    private static final String PROJECT = "all"; // try "weurope" or "canada"
-    private static final String API_URL = "https://my-api.plantnet.org/v2/identify/" + PROJECT + "?api-key=";
-    private static final String API_PRIVATE_KEY = "2b103ZERjEsjWMFUuTa2onvT"; // secret
-    private static final String API_SIMSEARCH_OPTION = "&include-related-images=true"; // optional: get most similar images
-    private static final String API_LANG = "&lang=en"; // default: en
-
-    public static void identifyPlant(File imageFile) {
-//        File imageFile = new File(imagePath);
-
+    public static void identifyPlant(final File imageFile) {
         if (!imageFile.exists()) {
             System.out.println("The specified image file does not exist.");
             return;
         }
 
-        RequestBody requestBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("images", imageFile.getName(), RequestBody.create(MediaType.parse("image/jpeg"), imageFile))
-                .addFormDataPart("organs", ORGAN)
-                .build();
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                RequestBody requestBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("images", imageFile.getName(), RequestBody.create(MediaType.parse("image/*"), imageFile))
+                        .addFormDataPart("organs", ORGAN)
+                        .build();
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(API_URL + API_PRIVATE_KEY)
-                .post(requestBody)
-                .build();
+                Request request = new Request.Builder()
+                        .url(URL)
+                        .post(requestBody)
+                        .build();
 
-        try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful()) {
-                String jsonString = response.body().string();
-                System.out.println(jsonString);
-            } else {
-                System.out.println("Request failed with code: " + response.code());
+                OkHttpClient client = new OkHttpClient();
+
+                try (Response response = client.newCall(request).execute()) {
+                    if (response.isSuccessful()) {
+                        return response.body().string();
+                    } else {
+                        return "HTTP request failed: " + response.code();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return "IOException occurred: " + e.getMessage();
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+            @Override
+            protected void onPostExecute(String result) {
+                System.out.println(result);
+                // Handle the response or update the UI here
+            }
+        }.execute();
     }
 }
-
