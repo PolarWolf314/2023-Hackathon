2e249d675d8bc44b0c77faae63151aa52dab8ef3
Anna Shimizu <ashi725@aucklanduni.ac.nz>
2023-07-08 17:34:14 +1200

    Added styling to ViewActivity

--- app/src/main/java/com/example/hackoverflow/ViewActivity.java
+++ app/src/main/java/com/example/hackoverflow/ViewActivity.java
@@ -2,7 +2,10 @@ package com.example.hackoverflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class ViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        Button viewBackButton = findViewById(R.id.viewBackButton);
+        viewBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    public void onBackPressed() {
+        Intent intent = new Intent(ViewActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
