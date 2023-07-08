f3c81b15b7500090823768e903d69cc7aa111485
Yang Qian <cyberdragon531@gmail.com>
2023-07-08 15:49:11 +1200

    Camera module working

--- app/src/main/java/com/example/hackoverflow/CameraActivity.java
+++ app/src/main/java/com/example/hackoverflow/CameraActivity.java
@@ -3,12 +3,19 @@ package com.example.hackoverflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import android.os.Bundle;
 
 public class CameraActivity extends AppCompatActivity {
+    Button btnpicture;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
     }
 }
