dd213a8c932ab1f9a873574bd5fa36a3aa14cb66
PolarWolf314 <aaronjoshguo@gmail.com>
2023-07-08 15:05:27 +1200

    feat - Added camera activity

--- app/src/main/java/com/example/hackoverflow/MainActivity.java
+++ app/src/main/java/com/example/hackoverflow/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.hackoverflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button getStartedButton = findViewById(R.id.mainGetStartedButton);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        getStartedButton.setOnClickListener(v -> {
+            // Handle button click event
+            Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+            startActivity(intent);
+        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
