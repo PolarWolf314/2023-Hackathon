c4a1b754d1712734a13afc1aadb28928803d7040
GBHU753 <100983240+GBHU753@users.noreply.github.com>
2023-07-09 13:08:39 +1200

    intital implementation of parsing
    initial implementation

--- app/src/main/java/com/example/hackoverflow/api.java
+++ app/src/main/java/com/example/hackoverflow/api.java
@@ -3,6 +3,15 @@ package com.example.hackoverflow;
 import java.io.File;
 import java.io.IOException;
 
+import android.content.res.AssetManager;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+
 import okhttp3.*;
 
 import android.os.AsyncTask;
@@ -48,9 +57,73 @@ public class api {
 
             @Override
             protected void onPostExecute(String result) {
-                System.out.println(result);
+                //System.out.println(result);
+                parseData(result);
                 // Handle the response or update the UI here
             }
         }.execute();
     }
+
+    public static void parseData(String jsonData){
+        // Assuming you have the JSON data in a String variable called jsonData
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray resultsArray = jsonObject.getJSONArray("results");
+
+            // Find the data with the highest score
+            double maxScore = 0;
+            JSONObject highestScoreData = null;
+            for (int i = 0; i < resultsArray.length(); i++) {
+                JSONObject result = resultsArray.getJSONObject(i);
+                double score = result.getDouble("score");
+                if (score > maxScore) {
+                    maxScore = score;
+                    highestScoreData = result;
+                }
+            }
+
+            if (highestScoreData != null) {
+                // Extract the required information from the highest score data
+                String scientificName = highestScoreData.getJSONObject("species").getString("scientificName");
+                // You can add more fields as per your JSON structure
+
+                // Use the extracted data as needed
+                System.out.println("Scientific Name: " + scientificName);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+    public static void writeToJson(){
+        try {
+            AssetManager manager = getAssets(); // Replace 'getAssets()' with your context's asset manager
+            InputStream file = manager.open("your_file_name.json");
+
+            int size = file.available();
+            byte[] buffer = new byte[size];
+            file.read(buffer);
+            file.close();
+
+            String jsonString = new String(buffer, "UTF-8");
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            // Create a new JSON object or array with the data you want to append
+            // For example, let's assume you have a new JSON object to append called 'newData'
+            jsonArray.put(newData);
+
+            // Write the updated JSON array back to the file
+            OutputStreamWriter outputWriter = new OutputStreamWriter(openFileOutput("your_file_name.json", Context.MODE_PRIVATE));
+            outputWriter.write(jsonArray.toString());
+            outputWriter.close();
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+     **/
+
 }
